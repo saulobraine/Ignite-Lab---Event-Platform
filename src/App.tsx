7ca2ea78f93b7b5db421a526bbaf6721c5b19d77
Saulo Braine <saulo@braine.dev>
2022-06-26 00:05:35 -0300

    Correção para build

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { ApolloProvider } from "@apollo/client";
 import { client } from "./lib/apollo";
-import { Router } from "./Router";
+import { Router } from "./router";
 
 function App() {
 
